dbad6fb2e403ca6f56dbf44b419d5b0bf5024cc4
andi <andiyael2208@gmail.com>
2022-12-23 03:58:25 -0600

    pagina

---
+++ nav.js
@@ -0,0 +1,25 @@
+const menu = document.getElementById('menu');
+const indicador = document.getElementById('indicador');
+const secciones = document.querySelectorAll('.seccion');
+let tamañoIndicador = menu.querySelector('a').offsetWidth;
+indicador.style.width = tamañoIndicador + 'px';
+let indexSeccionActiva;
+const observer = new IntersectionObserver((entradas, observer) => {
+	entradas.forEach(entrada => {
+		if(entrada.isIntersecting){
+		indexSeccionActiva = [...secciones].indexOf(entrada.target);
+			indicador.style.transform = `translateX(${tamañoIndicador * indexSeccionActiva}px)`;
+		}
+	});
+}, {
+	rootMargin: '-80px 0px 0px 0px',
+	threshold: 0.2
+});
+secciones.forEach(seccion => observer.observe(seccion));
+const onResize = () => {
+	tamañoIndicador = menu.querySelector('a').offsetWidth;
+	indicador.style.width = `${tamañoIndicador}px`;
+	indicador.style.transform = `translateX(${tamañoIndicador * indexSeccionActiva}px)`;
+}
+
+window.addEventListener('resize', onResize);
